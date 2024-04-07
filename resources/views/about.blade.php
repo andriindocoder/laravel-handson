767829514e51315443cde5997f8078e7aaf9d192
andriw <andriwicaksonost@gmail.com>
2024-04-07 12:46:44 +0700

    Make a Pretty Layout using TailwindCSS
    tailwindui

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
+	<x-slot:heading>
+        About Page
+    </x-slot:heading>
 	Hello from the About Page
 </x-layout>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
+	<x-slot:heading>
+        Contact Page
+    </x-slot:heading>
 	Hello from the Contact Page
 </x-layout>
